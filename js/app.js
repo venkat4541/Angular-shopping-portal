6bfa0dd4c86eb6e5b62a35aa51fac7037f6f352e
Venkat Polumahanti <venkat4541@gmail.com>
2017-04-08 22:44:10 -0400

    Basic structure ready

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 var app = angular.module('shop',[]);
 
 app.controller('ShopCtrl', ['$scope', function($scope) {
-  $scope.test = "Angular works!";
+  $scope.numbers = [
+    {
+      "itemName": "banana",
+      "imgSrc": "https://tinyurl.com/zcdrymz",
+      "price": 1.25,
+      "quantityRemaining": 10
+    }
+  ];
 }]);
